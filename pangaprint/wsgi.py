de12b690915278a34495408892145fd33171030d
--global <jeanmariesolutions@gmail.com>
2026-05-10 22:29:54 +0300

    Run migrations on app startup to fix database tables

--- pangaprint/wsgi.py
+++ pangaprint/wsgi.py
@@ -10,7 +10,11 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pangaprint.settings')
+
+# Run migrations on startup to ensure database is ready
+call_command('migrate', verbosity=0, interactive=False)
 
 application = get_wsgi_application()
